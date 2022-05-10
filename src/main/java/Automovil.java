6b8f4eaee4c7cd935011b585999a59607b9895f7
jppinedal <54005283+jppinedal@users.noreply.github.com>
2022-05-10 13:12:17 -0500

    T6, Jv, P4

--- src/main/java/Automovil.java
+++ src/main/java/Automovil.java
@@ -1,28 +1,27 @@
 package vehiculos;
 
-public class Automovil extends Vehiculo {
-	
-	private int puestos;
-	private static int cantidadAutomoviles = 0;
-	
-	
-	
-	public Automovil(String placa, String nombre, int peso, int precio, Fabricante fabricante, int puestos) {
-		
-		super(placa, nombre, 4, 100, precio, peso, fabricante, "FWD");
-		this.puestos = puestos;
-		cantidadAutomoviles++;
-		
-	}
-	
-	public void setPuestos(int puestos) {
+public class Automovil extends Vehiculo{
+
+    private static int cantidadAutomoviles = 0;
+    private int puestos;
+
+    public Automovil(String placa, String nombre, int peso, int precio, Fabricante fabricante, int puestos){
+
+        super(placa, nombre, 4, 100, precio, peso, fabricante, "FWD");
         this.puestos = puestos;
+        cantidadAutomoviles++;
+
     }
-	public static int getCantidadAutomoviles() {
+
+    public static int getCantidadAutomoviles() {
         return cantidadAutomoviles;
     }
+
     public int getPuestos() {
         return puestos;
     }
 
+    public void setPuestos(int puestos) {
+        this.puestos = puestos;
+    }
 }
--- src/main/java/Vehiculo.java
+++ src/main/java/Vehiculo.java
@@ -3,124 +3,120 @@ package vehiculos;
 import java.util.ArrayList;
 
 public class Vehiculo {
-	private String placa;
-	private int puertas;
-	private double velocidadMaxima;
-	private String nombre;
-	private int precio;
-	private int peso;
-	private String traccion;
-	private Fabricante fabricante; 
-	protected static int cantidadVehiculos = 0;
-	private static ArrayList<Vehiculo> vehiculos = new ArrayList<>();
-	
-	public Vehiculo(String placa, int puertas, double velocidadMaxima, String nombre, int precio, int peso, String traccion, Fabricante fabricante) {
-		this.placa = placa;
-		this.puertas = puertas;
-		this.velocidadMaxima = velocidadMaxima;
-		this.nombre = nombre;
-		this.precio = precio;
-		this.peso = peso;
-		this.traccion = traccion;
-		this.fabricante = fabricante;
-		cantidadVehiculos++;
-		vehiculos.add(this);
-	}
-	
-	public Vehiculo(String placa, String nombre, int puertas, int velocidadMaxima, int precio, int peso, Fabricante fabricante, String traccion) {
-		this.placa = placa;
-		this.nombre = nombre;
-		this.puertas = puertas;
-		this.velocidadMaxima = velocidadMaxima;
-		this.precio = precio;
-		this.peso = peso;
-		this.fabricante = fabricante;
-		this.traccion = traccion;
-		cantidadVehiculos++;
+
+    private static ArrayList<Vehiculo> vehiculos = new ArrayList<>();
+    protected static int cantidadVehiculos = 0;
+    private String placa;
+    private int puertas;
+    private int velocidadMaxima;
+    private String nombre;
+    private int precio;
+    private int peso;
+    private String traccion;
+    private Fabricante fabricante;
+
+    public Vehiculo(String placa, String nombre, int puertas, int velocidadMaxima, int precio, int peso, Fabricante fabricante, String traccion) {
+
+        this.puertas = puertas;
+        this.velocidadMaxima = velocidadMaxima;
+        this.traccion = traccion;
+        this.placa = placa;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.peso = peso;
+        this.fabricante = fabricante;
+        cantidadVehiculos++;
         vehiculos.add(this);
-	}
 
-	public String getPlaca() {
-		return placa;
-	}
+    }
 
-	public void setPlaca(String placa) {
-		this.placa = placa;
-	}
+    public Vehiculo(String placa, int puertas, int velociadadMaxima, String nombre, int precio, int peso, String traccion, Fabricante fabricante){
 
-	public int getPuertas() {
-		return puertas;
-	}
+        this(placa, nombre, puertas, velociadadMaxima, precio, peso, fabricante, traccion);
+        cantidadVehiculos++;
+        vehiculos.add(this);
 
-	public void setPuertas(int puertas) {
-		this.puertas = puertas;
-	}
+    }
 
-	public double getVelocidadMaxima() {
-		return velocidadMaxima;
-	}
+    public String vehiculosPorTipo(){
+        return  "Automoviles: " + Automovil.getCantidadAutomoviles() +
+                "\nCamionetas: " + Camioneta.getCantidadCamionetas() +
+                "\nCamiones: " + Camion.getCantidadCamiones();
+    }
 
-	public void setVelocidadMaxima(double velocidadMaxima) {
-		this.velocidadMaxima = velocidadMaxima;
-	}
+    public static ArrayList<Vehiculo> getVehiculos() {
+        return vehiculos;
+    }
 
-	public String getNombre() {
-		return nombre;
-	}
+    public static int getCantidadVehiculos() {
+        return cantidadVehiculos;
+    }
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
+    public static void setCantidadVehiculos(int cantidadVehiculos) {
+        Vehiculo.cantidadVehiculos = cantidadVehiculos;
+    }
 
-	public int getPrecio() {
-		return precio;
-	}
+    public String getPlaca() {
+        return placa;
+    }
 
-	public void setPrecio(int precio) {
-		this.precio = precio;
-	}
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
 
-	public int getPeso() {
-		return peso;
-	}
+    public int getPuertas() {
+        return puertas;
+    }
 
-	public void setPeso(int peso) {
-		this.peso = peso;
-	}
+    public void setPuertas(int puertas) {
+        this.puertas = puertas;
+    }
 
-	public String getTraccion() {
-		return traccion;
-	}
+    public int getVelocidadMaxima() {
+        return velocidadMaxima;
+    }
 
-	public void setTraccion(String traccion) {
-		this.traccion = traccion;
-	}
+    public void setVelocidadMaxima(int velocidadMaxima) {
+        this.velocidadMaxima = velocidadMaxima;
+    }
 
-	public Fabricante getFabricante() {
-		return fabricante;
-	}
+    public String getNombre() {
+        return nombre;
+    }
 
-	public void setFabricante(Fabricante fabricante) {
-		this.fabricante = fabricante;
-	}
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
-	public int getCantidadVehiculos() {
-		return cantidadVehiculos;
-	}
+    public int getPrecio() {
+        return precio;
+    }
 
-	public void setCantidadVehiculos(int cantidadVehiculos) {
-		Vehiculo.cantidadVehiculos = cantidadVehiculos;
-	}
-	
-	public static ArrayList<Vehiculo> getVehiculos(){
-		return vehiculos;
-	}
-	
-	
-	 public String vehiculosPorTipo(){
-	        return  "Automoviles: " + Automovil.getCantidadAutomoviles() +
-	                "\nCamionetas: " + Camioneta.getCantidadCamionetas() +
-	                "\nCamiones: " + Camion.getCantidadCamiones();
-	    }
-	
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public String getTraccion() {
+        return traccion;
+    }
+
+    public void setTraccion(String traccion) {
+        this.traccion = traccion;
+    }
+
+    public Fabricante getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(Fabricante fabricante) {
+        this.fabricante = fabricante;
+    }
 }
